676b841c91efb7ca06a745648abb760f4291efc4
Kalam Pinjar <owaiskal57@gmail.com>
2024-05-25 16:01:18 +0530

    finialized

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,215 +1,170 @@
-window.addEventListener("load", () => {
-  const canvas = document.getElementById("canvas1");
-  const ctx = canvas.getContext("2d");
-  canvas.width = 150;
-  canvas.height = 150;
 
-  class Particle {
-    constructor(effect, x, y, color) {
-      this.effect = effect;
-      this.x = Math.random() * this.effect.width;
-      this.y = 0;
-      this.originX = Math.floor(x);
-      this.originY = Math.floor(y);
-      this.color = color;
-      this.size = this.effect.gap;
-      this.vx = 0;
-      this.vy = 0;
-      this.ease = 0.05;
-      this.friction = 0.8;
-      this.dx = 0;
-      this.dy = 0;
-      this.distance = 0;
-      this.force = 0;
-      this.angle = 0;
-    }
-    draw(context) {
-      context.fillStyle = this.color;
-      context.fillRect(this.x, this.y, this.size, this.size);
-    }
-    update() {
-      this.dx = this.effect.mouse.x - this.x;
-      this.dy = this.effect.mouse.y - this.y;
 
-      this.distance = this.dx * this.dx + this.dy * this.dy;
-      this.force = -this.effect.mouse.radius / this.distance;
+window.addEventListener('load', () => {
+    const canvas = document.getElementById("canvas1")
+    const ctx = canvas.getContext('2d')
+    canvas.width = 150
+    canvas.height = 150
 
-      if (this.distance < this.effect.mouse.radius) {
-        this.angle = Math.atan2(this.dy, this.dx);
-        this.vx += this.force * Math.cos(this.angle);
-        this.vy += this.force * Math.sin(this.angle);
-      }
-
-      this.x +=
-        (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
-      this.y +=
-        (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
-    }
-    wrap() {
-      this.x = Math.random() * this.effect.width;
-      this.y = Math.random() * this.effect.height;
-      this.ease = 0.05;
-    }
-  }
-
-  class Effect {
-    constructor(width, height) {
-      this.width = width;
-      this.height = height;
-      this.particlesArray = [];
-      this.image1 = document.getElementById("image1");
-      this.centerX = this.width * 0.5;
-      this.centerY = this.height * 0.5;
-      this.imgsize = 200;
-      this.x = this.centerX - this.imgsize * 0.5;
-      this.y = this.centerY - this.imgsize * 0.5;
-      // console.log(image1.width)
-      // console.log(image1.height)
-      this.gap = 2;
-      this.mouse = {
-        radius: 3000,
-        x: undefined,
-        y: undefined,
-      };
-      window.addEventListener("mousemove", (e) => {
-        this.mouse.x = e.x;
-        this.mouse.y = e.y;
-      });
-      window.addEventListener("mouseout", (e) => {
-        this.mouse.x = undefined;
-        this.mouse.y = undefined;
-      });
-    }
-    init(context) {
-      context.drawImage(
-        this.image1,
-        this.x,
-        this.y,
-        this.imgsize,
-        this.imgsize
-      );
-      const pixels = context.getImageData(0, 0, this.width, this.height).data;
-      // console.log(pixels)
-      for (let y = 0; y < this.height; y += this.gap) {
-        for (let x = 0; x < this.width; x += this.gap) {
-          const index = (y * this.width + x) * 4;
-          const red = pixels[index];
-          const green = pixels[index + 1];
-          const blue = pixels[index + 2];
-          const alpha = pixels[index + 3];
-          const color = `rgb(${red},${green},${blue})`;
-
-          if (alpha > 0) {
-            this.particlesArray.push(new Particle(this, x, y, color));
-          }
+    class Particle {
+        constructor(effect, x, y, color) {
+            this.effect = effect
+            this.x = Math.random() * this.effect.width
+            this.y = 0
+            this.originX = Math.floor(x)
+            this.originY = Math.floor(y)
+            this.color = color
+            this.size = this.effect.gap
+            this.vx = 0
+            this.vy = 0
+            this.ease = 0.05
+            this.friction = 0.8
+            this.dx = 0
+            this.dy = 0
+            this.distance = 0
+            this.force = 0
+            this.angle = 0
         }
-      }
+        draw(context) {
+            context.fillStyle = this.color
+            context.fillRect(this.x, this.y, this.size, this.size)
+        }
+        update() {
+            this.dx = this.effect.mouse.x - this.x
+            this.dy = this.effect.mouse.y - this.y
+
+            this.distance = this.dx * this.dx + this.dy * this.dy
+            this.force = -this.effect.mouse.radius / this.distance
+
+            if (this.distance < this.effect.mouse.radius) {
+                this.angle = Math.atan2(this.dy, this.dx)
+                this.vx += this.force * Math.cos(this.angle)
+                this.vy += this.force * Math.sin(this.angle)
+            }
+
+            this.x += (this.vx *= this.friction) + (this.originX - this.x) * this.ease
+            this.y += (this.vy *= this.friction) + (this.originY - this.y) * this.ease
+        }
+        wrap() {
+
+            this.x = Math.random() * this.effect.width
+            this.y = Math.random() * this.effect.height
+            this.ease = 0.05
+        }
     }
-    draw(context) {
-      this.particlesArray.forEach((particle) => {
-        particle.draw(context);
-      });
+
+    class Effect {
+        constructor(width, height) {
+            this.width = width
+            this.height = height
+            this.particlesArray = []
+            this.image1 = document.getElementById('image1')
+            this.centerX = this.width * 0.5
+            this.centerY = this.height * 0.5
+            this.imgsize = 200
+            this.x = this.centerX - this.imgsize * 0.5
+            this.y = this.centerY - this.imgsize * 0.5
+            // console.log(image1.width)
+            // console.log(image1.height)
+            this.gap = 2
+            this.mouse = {
+                radius: 3000,
+                x: undefined,
+                y: undefined
+            }
+            window.addEventListener('mousemove', e => {
+                this.mouse.x = e.x
+                this.mouse.y = e.y
+            })
+            window.addEventListener('mouseout', e => {
+                this.mouse.x = undefined
+                this.mouse.y = undefined
+            })
+        }
+        init(context) {
+            context.drawImage(this.image1, this.x, this.y, this.imgsize, this.imgsize)
+            const pixels = context.getImageData(0, 0, this.width, this.height).data
+            // console.log(pixels)
+            for (let y = 0; y < this.height; y += this.gap) {
+                for (let x = 0; x < this.width; x += this.gap) {
+                    const index = (y * this.width + x) * 4
+                    const red = pixels[index]
+                    const green = pixels[index + 1]
+                    const blue = pixels[index + 2]
+                    const alpha = pixels[index + 3]
+                    const color = `rgb(${red},${green},${blue})`
+
+                    if (alpha > 0) {
+                        this.particlesArray.push(new Particle(this, x, y, color))
+                    }
+                }
+            }
+        }
+        draw(context) {
+            this.particlesArray.forEach(particle => {
+                particle.draw(context)
+
+            })
+
+        }
+        update() {
+            this.particlesArray.forEach(particle => {
+                particle.update()
+            })
+        }
+        // wrap() {
+        //     this.particlesArray.forEach(particle => {
+        //         particle.wrap()
+        //     })
+        // }
     }
-    update() {
-      this.particlesArray.forEach((particle) => {
-        particle.update();
-      });
+
+
+    const effect = new Effect(canvas.width, canvas.height)
+    effect.init(ctx)
+    // console.log(effect.particlesArray)
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        effect.draw(ctx)
+        effect.update()
+        requestAnimationFrame(animate)
     }
-    // wrap() {
-    //     this.particlesArray.forEach(particle => {
-    //         particle.wrap()
-    //     })
-    // }
-  }
+    animate()
 
-  const effect = new Effect(canvas.width, canvas.height);
-  effect.init(ctx);
-  // console.log(effect.particlesArray)
 
-  function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    effect.draw(ctx);
-    effect.update();
-    requestAnimationFrame(animate);
-  }
-  animate();
 
-  const tl = gsap.timeline({ default: { duration: 1, ease: Expo.easeInOut } });
-  tl.to(
-    ".header",
-    {
-      opacity: 1,
-    },
-    "a"
-  )
-    .from(
-      ".navbar",
-      {
-        top: -100,
-        opacity: 0,
-      },
-      "a"
-    )
-    .to(
-      ".navbar",
-      {
-        opacity: 1,
-        top: 0,
-        duration: 1,
-        ease: Expo.easeInOut,
-        opacity: 1,
-      },
-      "a"
-    );
+    const tl = gsap.timeline({ default: { duration: 1, ease: Expo.easeInOut, } })
+    tl.to(".header", {
+        opacity: 1
+    }, 'a')
+        .from(".navbar", {
+            top: -100,
+            opacity: 0
+        }, 'a').to('.navbar', {
+            opacity: 1,
+            top: 0,
+            duration: 1,
+            ease: Expo.easeInOut,
+            opacity: 1,
+        }, 'a')
 
-  //smooth scroll
-  const lenis = new Lenis();
 
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
+    //smooth scroll
+    const lenis = new Lenis()
 
-  requestAnimationFrame(raf);
-});
 
-const avatarButton = document.getElementById("avatarButton");
-const userDropdown = document.getElementById("userDropdown");
-avatarButton.addEventListener("click", () => {
-  userDropdown.classList.toggle("userDropdownShow");
-});
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
 
-(function () {
-  emailjs.init("J0NGwqJFRtwcv06DA");
-})();
-// Function to send email
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
+    requestAnimationFrame(raf)
+})
 
-    let email = document.getElementById("email").value;
-    let name = document.getElementById("name").value;
-    let message = document.getElementById("message").value;
-    console.log(email, name, message);
-    // Set up email data
-    const templateParams = {
-      from_email: email,
-      from_name: name,
-      message: message,
-    };
-
-    // Send email
-    emailjs
-      .send("service_nys5n4e", "template_p5j55ek", templateParams)
-      .then((response) => {
-        console.log("Email successfully sent!", response, response.text);
-
-        setTimeout(() => {
-          document.getElementById("contact-form").reset();
-        }, 1000);
-      })
-      .catch((error) => {
-        console.error("Failed to send email: ", error);
-      });
-  });
+const avatarButton = document.getElementById("avatarButton")
+const userDropdown = document.getElementById("userDropdown")
+avatarButton.addEventListener('click', function(){
+    userDropdown.classList.toggle("userDropdownShow")
+    console.log("clicked")
+})
---
+++ history.js
@@ -0,0 +1,193 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { getDatabase, ref, set, onValue, push, update, remove, get, child } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+import { getStorage, ref as sref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
+import { getFirestore, collection, addDoc, doc, setDoc, Timestamp, getDocs, query, orderBy, deleteDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCxRzkNAyNIr6GnJE7GGic2kOJh_xQoQQ0",
+    authDomain: "cipherweb-a2c6b.firebaseapp.com",
+    projectId: "cipherweb-a2c6b",
+    storageBucket: "cipherweb-a2c6b.appspot.com",
+    messagingSenderId: "169319433436",
+    appId: "1:169319433436:web:3f420e647573d50e6fe019",
+    measurementId: "G-KL8BY17RQH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const user = auth.currentUser;
+
+const database = getDatabase(app);
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const storage = getStorage(app);
+// Create a storage reference from our storage service
+const dbstore = getFirestore(app);
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, fetch their history
+        const userHistoryRef = collection(dbstore, 'users', user.uid, 'history');
+        const orderedHistoryRef = query(userHistoryRef, orderBy('Date', 'desc'));
+        getDocs(orderedHistoryRef).then((querySnapshot) => {
+
+            // doc.data() is never undefined for query doc snapshots
+            // console.log(doc.id, " => ", doc.data());
+            // Here, you can append the data to your HTML to display it
+            const historyData = querySnapshot.docs.map(doc => doc.data());
+            displayHistory(historyData);
+
+
+        }).catch((error) => {
+            console.error("Error getting documents: ", error);
+        });
+    }
+});
+const historyHolder = document.querySelector(".history-holder");
+
+
+function displayHistory(historyData) {
+    const historyHolder = document.querySelector(".history-holder");
+    historyHolder.innerHTML = '';
+
+    if (historyData.length === 0) {
+        // If there's no history, display a message
+        historyHolder.innerHTML = `
+            <center><p class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400 underline">No Activity Found</p></center>
+        `;
+    }
+    else {
+
+
+        historyData.forEach(item => {
+            let historyItemHtml = `
+                    <li class="mb-10 ms-4">
+                    <div
+                        class="absolute border-white dark:border-gray-900 bg-gray-200 dark:bg-gray-700 mt-1.5 border rounded-full w-3 h-3 -start-1.5">
+                    </div>
+                    <time id="timestamp"
+                        class="mb-1 font-normal text-gray-400 text-sm dark:text-gray-500 leading-none">${item.Date.toDate()}</time>
+                    <h3 id="Algorithm-title" class="font-semibold text-gray-900 text-lg dark:text-white">${item.AlgorithmTitle}
+                    </h3>
+                    `;
+
+            if (item.plainText) {
+                historyItemHtml += `<p id="plaintext" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">plainText: ${item.plainText}</p>`;
+            }
+            if (item.mode) {
+                historyItemHtml += `<p id="mode" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">mode: ${item.mode}</p>`;
+            }
+            if (item.key) {
+                historyItemHtml += `<p id="key" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">key: ${item.key}</p>`;
+            }
+            if (item.cipherText) {
+                historyItemHtml += `<p id="ecoded-text" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">cipherText: ${item.cipherText}</p>`;
+            }
+            if (item.encryptedText) {
+                historyItemHtml += `<p id="encryptedText" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">encryptedText: ${item.encryptedText}</p>
+                `
+            }
+            if (item.decryptedText) {
+                historyItemHtml += `<p id="decryptedText" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">decryptedText: ${item.decryptedText}</p>
+                `
+            }
+            if (item.Decryptoutput) {
+                historyItemHtml += `<p id="Decryptoutput" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">Decryptoutput: ${item.Decryptoutput}</p>
+                `
+            }
+            if (item.encodedText) {
+                historyItemHtml += `<p id="encodedText" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">encodedText: ${item.encodedText}</p>
+                `
+            }
+            if (item.decodedText) {
+                historyItemHtml += `<p id="decodedText" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">decodedText: ${item.decodedText}</p>
+                `
+            }
+            if (item.privateKey) {
+                historyItemHtml += `<p id="private-key" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">privateKey: ${item.privateKey}</p>
+                `
+            }
+            if (item.publicKey) {
+                historyItemHtml += `<p id="public-key" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">publicKey: ${item.publicKey}</p>
+                `
+            }
+            if (item.SHA256Hash) {
+                historyItemHtml += `<p id="SHA256Hash" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">SHA256Hash: ${item.SHA256Hash}</p>
+                `
+            }
+            if (item.Output) {
+                historyItemHtml += `<p id="Output" class="mb-4 font-normal text-base text-gray-500 dark:text-gray-400">Output: ${item.Output}</p>
+                `
+
+            }
+            historyItemHtml += `</li>`;
+
+            historyHolder.innerHTML += historyItemHtml;
+
+
+
+        })
+    }
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        const deleteData = document.querySelector("#deleteData");
+        // Add event listener to the delete icon
+        deleteData.addEventListener("click", () => {
+            // Get a reference to the user's history collection
+            const userHistoryRef = collection(dbstore, 'users', user.uid, 'history');
+
+            // Delete all documents in the history collection
+            getDocs(userHistoryRef).then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    deleteDoc(doc.ref).then(() => {
+                        const toastMsgSignOut = document.getElementById("toast-signout")
+
+                        toastMsgSignOut.innerHTML = `<div
+class="inline-flex flex-shrink-0 justify-center items-center bg-blue-100 dark:bg-blue-800 rounded-lg w-8 h-8 text-blue-500 dark:text-blue-200">
+<svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+    d="M15.147 15.085a7.159 7.159 0 0 1-6.189 3.307A6.713 6.713 0 0 1 3.1 15.444c-2.679-4.513.287-8.737.888-9.548A4.373 4.373 0 0 0 5 1.608c1.287.953 6.445 3.218 5.537 10.5 1.5-1.122 2.706-3.01 2.853-6.14 1.433 1.049 3.993 5.395 1.757 9.117Z" />
+</svg>
+<span class="sr-only">Fire icon</span>
+</div>
+<div class="font-normal text-sm ms-3">Data Deleted Successfully!</div>
+<button type="button"
+class="inline-flex justify-center items-center bg-white hover:bg-gray-100 dark:hover:bg-gray-700 dark:bg-gray-800 -mx-1.5 -my-1.5 p-1.5 rounded-lg w-8 h-8 text-gray-400 hover:text-gray-900 dark:hover:text-white ms-auto focus:ring-2 focus:ring-gray-300 dark:text-gray-500"
+data-dismiss-target="#toast-default" aria-label="Close">
+<span class="sr-only">Close</span>
+<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+</svg>
+</button>`
+                        toastMsgSignOut.classList.add("toastanime")
+
+                        setTimeout(() => {
+                            toastMsgSignOut.classList.remove("toastanime")
+                        }, 3000)
+
+                        // Optionally, refresh the history display
+                        displayHistory([]); // Clear the history display
+                    }).catch((error) => {
+                        console.error("Error deleting history document: ", error);
+                    });
+                });
+            }).catch((error) => {
+                console.error("Error getting documents: ", error);
+            });
+        });
+    }
+    else {
+        window.location = "/Login/Form.html"
+    }
+})
